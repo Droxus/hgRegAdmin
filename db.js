51cb9afc92b6b896b0480bda20d43a4a72ee6167
Droxus <alexredov@gmail.com>
2024-09-04 15:39:58 +0100

    env deleted

--- db.js
+++ db.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SEDNER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBwTbTG_auMrWy_e4aVUFMHs3794zZ4AXE",
+  authDomain: "hgregusersdb.firebaseapp.com",
+  projectId: "hgregusersdb",
+  storageBucket: "hgregusersdb.appspot.com",
+  messagingSenderId: "996459139126",
+  appId: "1:996459139126:web:c4f73b0298e90734618897",
+  measurementId: "G-J4RYP9B18W",
 };
 
 // Initialize Firebase
