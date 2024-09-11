67d13a53a29f466da7d5f621bb89e8c4aea7bc1b
Droxus <alexredov@gmail.com>
2024-09-11 16:51:27 +0100

    react added db updated, auth fixed

--- db.js
+++ db.js
@@ -1,179 +1,91 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  deleteDoc,
-  updateDoc,
-} from "firebase/firestore";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  onAuthStateChanged,
-} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as Firestore from "firebase/firestore";
+import * as Auth from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBwTbTG_auMrWy_e4aVUFMHs3794zZ4AXE",
-  authDomain: "hgregusersdb.firebaseapp.com",
-  projectId: "hgregusersdb",
-  storageBucket: "hgregusersdb.appspot.com",
-  messagingSenderId: "996459139126",
-  appId: "1:996459139126:web:c4f73b0298e90734618897",
-  measurementId: "G-J4RYP9B18W",
-};
+export default class DataBase {
+  app;
+  db;
+  analytics;
+  auth;
+  provider;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
+  #data;
+  #bin;
 
-const auth = getAuth(app);
+  constructor() {
+    this.app = initializeApp(
+      JSON.parse(atob(import.meta.env.VITE_FIREBASE_TOKEN))
+    );
+    this.db = Firestore.getFirestore(this.app);
+    this.analytics = getAnalytics(this.app);
+    this.auth = Auth.getAuth(this.app);
+    this.provider = new Auth.GoogleAuthProvider();
 
-// Set up Google provider
-const provider = new GoogleAuthProvider();
+    this.#data = {};
+    this.#bin = {};
+  }
 
-// Sign in with Google
-export async function signInWithGoogle() {
-  return new Promise((resolve) => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        resolve(true);
-      })
-      .catch((error) => {
-        resolve(false);
-      });
-    resolve(false);
-  });
-}
-
-// Check if user is already logged in
-export async function checkUserLoginStatus() {
-  return new Promise((resolve) => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-  });
-}
-
-const RESERVED_ID = "test";
-
-const data = {
-  imported: false,
-  services: {
-    advanced: {},
-    basic: {},
-    ultra: {},
-    others: {},
-  },
-};
-
-const bin = {
-  imported: false,
-  services: {
-    advanced: {},
-    basic: {},
-    ultra: {},
-    others: {},
-  },
-};
-
-async function getCollection(path) {
-  const thisCollection = {};
-  const querySnapshot = await getDocs(collection(db, path));
-  querySnapshot.forEach((doc) => {
-    const id = doc.id;
-    if (id !== RESERVED_ID) {
-      thisCollection[id] = doc.data();
+  async signInWithGoogle() {
+    try {
+      await Auth.signInWithPopup(this.auth, this.provider);
+      return true;
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+      return false;
     }
-  });
-
-  return thisCollection;
-}
-
-export async function getData(force = false) {
-  if (data.imported && !force) {
-    return data;
   }
 
-  data.services.advanced = await getCollection("Data/Services/Advanced");
-  data.services.basic = await getCollection("Data/Services/Basic");
-  data.services.ultra = await getCollection("Data/Services/Ultra");
-  data.services.others = await getCollection("Data/Services/Others");
-  data.imported = true;
-
-  return data;
-}
-
-export async function getBin() {
-  if (bin.imported) {
-    return bin;
-  }
-
-  bin.services.advanced = await getCollection("Bin/Services/Advanced");
-  bin.services.basic = await getCollection("Bin/Services/Basic");
-  bin.services.ultra = await getCollection("Bin/Services/Ultra");
-  bin.services.others = await getCollection("Bin/Services/Others");
-  bin.imported = true;
-
-  return bin;
-}
-
-export async function deleteForm(id) {
-  const thisDoc = getDocRef(id);
-
-  try {
-    await deleteDoc(thisDoc);
-    getData(true);
-  } catch (error) {
-    console.error(
-      `Failed to delete document with ID: ${id} from collection.`,
-      error
+  async checkUserLoginStatus() {
+    return new Promise((resolve) =>
+      Auth.onAuthStateChanged(this.auth, (user) => resolve(!!user))
     );
   }
-}
 
-export async function editForm(id, newForm) {
-  const thisForm = getDocRef(id);
+  async getCollection(path) {
+    const collection = Firestore.collection(this.db, path);
+    const querySnapshot = await Firestore.getDocs(collection);
 
-  try {
-    await updateDoc(thisForm, newForm);
-    getData(true);
-  } catch (error) {
-    console.error(`Failed to edit document with ID: ${id}.`, error);
+    return querySnapshot.docs.reduce((acc, doc) => {
+      acc[doc.id] = doc.data();
+      return acc;
+    }, {});
   }
-}
 
-function getDocRef(id) {
-  const services = data.services;
-  const collections = ["advanced", "basic", "ultra", "others"];
-  let collection = "";
-  let keyCollecation = "";
+  async updateData() {
+    this.#data = await this.getCollection("Data");
+    this.#bin = await this.getCollection("Bin");
+  }
 
-  for (const key of collections) {
-    if (Object.keys(services[key]).includes(id)) {
-      keyCollecation = key;
-      collection = key.charAt(0).toUpperCase() + key.slice(1); // Capitalize the first letter
-      break;
+  async deleteDoc(id) {
+    const collection = "Data";
+    const thisDoc = Firestore.doc(this.db, collection, id);
+
+    try {
+      await Firestore.deleteDoc(thisDoc);
+      this.updateData();
+    } catch (error) {
+      console.error(`Failed to delete document with ID: ${id}.`, error);
     }
   }
 
-  if (!collection) {
-    throw new Error(`No matching collection found for ID: ${id}`);
+  async editDoc(id, newDoc) {
+    const collection = "Data";
+    const thisDoc = Firestore.doc(this.db, collection, id);
+
+    try {
+      await Firestore.updateDoc(thisDoc, newDoc);
+      this.updateData();
+    } catch (error) {
+      console.error(`Failed to edit document with ID: ${id}.`, error);
+    }
   }
 
-  const path = `Data/Services/${collection}`;
-  const thisDoc = doc(db, path, id);
-  return thisDoc;
+  get data() {
+    return this.#data;
+  }
+
+  get bin() {
+    return this.#bin;
+  }
 }
